5f634f980801154cb6538abf8a907f06c70c1f57
Anthony Rivard <anthonywrivard@gmail.com>
2017-02-07 20:32:41 -0500

    created css and html parts

---
+++ tony.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<!--
+Anthony Rivard
+CSCI E-15 Assignment #1
+-->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CSCI E-15 Rivard-1</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<div class="container">
+
+			<h1>Anthony Rivard</h1>
+
+			<img src="images/tony.jpg" width="198" height="417" alt=""/>
+
+			<p> Hello my name is Anthony Rivard and this is my second class at Harvard Extension School. I live in Michigan and work at The Dow Chemical Company. I work in IT asset management mainly tracking Microsoft assets throughout the company. I have my undergrad in Information Management and an MBA from Northwood University. I also have a Master’s in Business Analytics from Michigan State. I enjoy taking out my telescope and doing astrophotography. I also like to golf, play softball, and go to rock concerts. </p>
+			
+			<h2>Random Quote</h2>
+			<blockquote></blockquote>
+		</div>
+	</body>
+</html>
